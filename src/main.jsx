3889a4e690152de2f438560e75714e3cfa216859
Shivam-Mishra20 <shivammishra912998@gmail.com>
2024-04-24 20:51:25 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux';
 import store from './Components/Redux/Store';
-import { HashRouter } from 'react-router-dom';
+ 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <HashRouter>
+     
       <App />
-    </HashRouter>
+     
 
 
 
